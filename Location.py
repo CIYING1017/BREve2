ea81e5454e361ca89c66e226ba852df31fb73b97
CIYING1017 <86265612+CIYING1017@users.noreply.github.com>
2022-08-22 09:47:02 +0100

    Update Location.py

--- Location.py
+++ Location.py
@@ -4,19 +4,21 @@ from tkinter.ttk import Label
 
 root = Tk()
 
+# creating title widget
+myLabel1 = Label (root, text = "Location info")
+myLabel2 = Label (root, text = "Put in coordinates or postcodes to obtain terrain and topographic data")
+
 # create entry widget
 # e = Entry (root, width = 50, borderwidth = 5)
 # e.pack()
 
 
 # define a function where when you click ,it responses with a sentence
-def myClick():
-    myLabel3 = Label (root, text ="Hello "+e.get())
-    myLabel3.pack()
+# def myClick():
+#     myLabel3 = Label (root, text ="Hello "+e.get())
+#     myLabel3.pack()
 
-# creating a label widget
-myLabel1 = Label (root, text = "Welcome to the BREVe2 wind wizard!")
-myLabel2 = Label (root, text = "Hello world!")
+
 root.bold25 = Font (root.master, size = 100, weight = BOLD)
 
 # myButton = Button  (root, text = "Enter your name", padx=50,pady=10, command = myClick, fg="blue", bg = "white")
